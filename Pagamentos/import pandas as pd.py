422acb7e178d7c08473792e79fdeefa32f526e81
Luizfsbessa <luizfsbessa@gmail.com>
2024-11-27 19:30:24 +0000

    Atualizar o nome e caminho do arquivo CSV

--- Pagamentos/import pandas as pd.py
+++ Pagamentos/import pandas as pd.py
@@ -1,3 +1,9 @@
 import pandas as pd
-df = pd.read_csv('pagamentos2025.csv')
-print(df.head())
+import time
+import os
+print("Diretório atual:", os.getcwd())
+start_time = time.time()
+print("Iniciando a leitura do arquivo...")
+df = pd.read_csv('pg25.csv')
+print("Arquivo lido com sucesso!")
+print(f"Tempo de execução: {time.time() - start_time} segundos")
